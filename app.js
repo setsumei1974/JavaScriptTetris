d58cccfb05522ab7721ddc6c4d1f0705efc653f9
setsumei1974 <david.kaatmann@gmail.com>
2020-08-30 00:17:37 -0500

    Tetris with Javascript

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartBtn = document.querySelector('#start-button')
   const width = 10
 
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
 })
